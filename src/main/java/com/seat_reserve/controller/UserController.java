1bdcbd725cec56e09ed9f3dff9e6c6c66d0b72d0
YoheiYamashita <120314392+YoheiYamashita@users.noreply.github.com>
2024-03-19 07:05:55 +0900

    ユーザーアイコン追加

--- src/main/java/com/seat_reserve/controller/UserController.java
+++ src/main/java/com/seat_reserve/controller/UserController.java
@@ -38,38 +38,40 @@ public class UserController {
 
 		//既にseatとdateが入ってたら、削除機能に遷移
 		List<Reserve> ListByDateAndSeat = reserveService.findByDateAndSeat(date, seat);
+		System.out.println("ListByDateAndSeatです："+ListByDateAndSeat);
 		//seatとdateがあったら削除機能、なければuser.htmlを介して予約機能
 		if (ListByDateAndSeat == null || ListByDateAndSeat.isEmpty()) {
 			model.addAttribute("seat", seat);
 			model.addAttribute("date", date);
-
 			//メンバー一覧を取得し、userlistオブジェクトに格納
 			List<User> userlist = userService.seachAll();
 			model.addAttribute("userlist", userlist);
+			System.out.println("ListByDateAndSeatです："+ListByDateAndSeat);
 			return "user";
 		} else {
 			//拡張for文で、ListByDateAndSeatの全ての値を取得
-//			for (Reserve reserve : ListByDateAndSeat) {
-//				Integer reserveId = reserve.getReserveId();
-//				System.out.println("ReserveIDです：" + reserveId);
-//
-//				//reserveIdで削除を実行
-//
-//			}
+			//			for (Reserve reserve : ListByDateAndSeat) {
+			//				Integer reserveId = reserve.getReserveId();
+			//				System.out.println("ReserveIDです：" + reserveId);
+			//
+			//				//reserveIdで削除を実行
+			//
+			//			}
 			//	
+			System.out.println("ListByDateAndSeatです："+ListByDateAndSeat);
+
 			model.addAttribute("listByDateAndSeat", ListByDateAndSeat);
 			// reserveIdsを空のリストとして追加する
 			model.addAttribute("reserveIds", new ArrayList<Integer>());
 
 			return "delete/deleteConfirmation.html";
 		}
-
 	}
-	
+
 	@PostMapping("deleteCompletion")
 	@Transactional
 	public String deleteReservation(@RequestParam List<Integer> reserveIds) {
-		for(Integer reserveId:reserveIds) {
+		for (Integer reserveId : reserveIds) {
 			reserveService.deleteByReserveId(reserveId);
 		}
 		return "delete/deleteCompletion.html";
